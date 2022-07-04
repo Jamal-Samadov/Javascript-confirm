f442b55f3ae76b101954e436d0fc91b9913a8c0f
Jamaltscode <jamalts@code.edu.az>
2022-07-04 09:30:54 +0400

    Confirm task done

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+var sure = confirm("Are you sure want to see my page?");
+ if(sure == true){
+    document.body.style.backgroundColor = 'grey'
+ }
+ else{
+    document.write('Sorry Hüseyn hocam, i can not show my page').style = 'blue'
+ }
+
+ 
